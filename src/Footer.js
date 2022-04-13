562d7d91c3ef2ed920ca69bbb99a81f79e5dff86
Fernando Gazolla <30941854+FernandoGazolla@users.noreply.github.com>
2022-04-12 21:52:06 -0300

    Criando Cards

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <footer className = "text-muted py-5">
+        <div className="container">
+         
+          <p className="mb-1">Gazolla você é Foda!</p>
+          
+        </div>
+      </footer>
+      )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import  'bootstrap/dist/css/bootstrap.css'
+
+import Main from './Main';
+import Footer from './Footer';
+import Header from './Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Main />
+    <Footer />
   </React.StrictMode>
 );
 
